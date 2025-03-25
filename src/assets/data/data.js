5e7c8758320357e1005ebe70724450710b839736
susahbangun <suhud4rt@gmail.com>
2025-03-26 00:57:16 +0700

    Update data.js

--- src/assets/data/data.js
+++ src/assets/data/data.js
@@ -1,4 +1,10 @@
 export const data = {
+    ],
+
+    api: 'https://script.google.com/macros/s/AKfycbwwfP5uQH1irDCpmTapdXijSnw8qrypbgY3BHfLP2XGGvTUcPe5Nt7TYTj2agdmpBIgRA/exec',
+
+    navbar: [
+        {
             id: 5,
             teks: 'Ucapan',
             icon: 'bx bxs-message-rounded-dots',
